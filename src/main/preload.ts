81a5e01140c1e6fa45ecb7250da4b8b057775c84
n-gist <58081918+n-gist@users.noreply.github.com>
2024-05-30 02:50:48 +0500

    Add 'once' to ipcRenderer

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -7,6 +7,11 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
     },
+    once(...args: Parameters<typeof ipcRenderer.once>) {
+        const [channel, listener] = args
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        return ipcRenderer.once(channel, (event, ...args) => listener(event, ...args))
+    },
     off(...args: Parameters<typeof ipcRenderer.off>) {
         const [channel, ...omit] = args
         return ipcRenderer.off(channel, ...omit)
